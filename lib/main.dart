7a534d37b61463bd596fc3def41d7a48ad47c86f
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-11-11 22:38:04 +0530

    new chnges done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/todo_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
+      home: TodoListPage(),
     );
   }
 }
