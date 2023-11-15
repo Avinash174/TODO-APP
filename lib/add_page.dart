74b11048a200fb0fee9564557838666f522e7f9d
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-11-15 17:07:43 +0530

    new changes donr

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -8,6 +8,8 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +17,35 @@ class _AddTodoPageState extends State<AddTodoPage> {
         title: const Text('Add Todo'),
         centerTitle: true,
       ),
-      body: ListView(),
+      body: ListView(
+        padding: const EdgeInsets.all(15),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Title'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextField(
+            controller: descriptionController,
+            minLines: 5,
+            maxLines: 5,
+            keyboardType: TextInputType.multiline,
+            decoration: const InputDecoration(
+              hintText: 'Description',
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+        ],
+      ),
     );
+  }
+
+  void submitData() {
+    //Get Data From Server
   }
 }
